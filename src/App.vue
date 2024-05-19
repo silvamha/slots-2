2d041a045c41e2998dc6ec155adb04aed8cab4be
Ed <silva.email@yahoo.com>
2024-05-19 17:37:28 -0500

    refactor: Optimize form structure in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
 /**
- * @file App.vue
- * @description The main component of the Vue.js application.
- */
- <template>
-<form>
-<div class="help">
-  <p>This is some help text.</p>
-</div>
-<div class="fields"
-<input type="text" placeholder="email">
-<input type="text" placeholder="username">
-<input type="password" placeholder="password">
-</div>
-<div class="buttons">
-  <button type="submit">Submit</button>
-</div>
-</form>
- </template>
+* @file App.vue
+* @description The main component of the Vue.js application.
+*/
+<template>
+<app-form />
+</template>
 
 
 <script>
-export default{
+import AppForm from './components/Form.vue'
+export default {
   name: 'App'
 }
+components: {
+  AppForm
+}
 </script>
-
-
-
-
